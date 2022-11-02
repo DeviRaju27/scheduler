e192ce9cf64ef3cf43f83cbc4ae3e0cc9cd73314
Devi Raju <deviraju999@gmail.com>
2022-11-01 23:51:19 -0700

    add state for current day

---
+++ src/components/DayListItem.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "components/DayListItem.scss";
+import classNames from "classnames";
+
+
+export default function DayListItem(props){
+
+  const setDay = (props) => {
+    props.selected = true
+  }
+
+  const dayclass = classNames("day-list__item",
+  {
+    "day-list__item--selected":props.selected,
+    "day-list__item--full":props.spots===0
+  });
+
+
+
+  const formatSpots = (a) => {
+    if(a === 0){
+      return `no spots remaining`
+    } else if(a === 1){
+      return `${a} spot remaining`
+    } else {
+     return  `${a} spots remaining`
+    }
+  }
+
+  return (
+    <li className={dayclass} onClick = {() => props.setDay(props.name)}>
+      <h2 >{props.name}</h2>
+      <h3 className={dayclass}>{formatSpots(props.spots)}</h3>
+    </li>
+  )
+}
